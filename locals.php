a5d77bb475daf4cde24134786ef8cf36c7f6def9
Andrei Lupu <euthelup@gmail.com>
2018-12-21 16:44:04 +0200

    update plugin description

--- locals.php
+++ locals.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name:     Locals
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin Name:     DashIcon Block
+ * Plugin URI:      https://a.lupu.pro
+ * Description:     Experimental Block for displaying WordPress dash icons in front-end.
+ * Author:          Andrei Lupu
+ * Author URI:      https://a.lupu.pro
  * Text Domain:     locals
  * Domain Path:     /languages
  * Version:         0.1.0
